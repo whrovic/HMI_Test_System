d646a1f273fbd7acd6638249ca90faa89fdf023b
Ricardo Silva <up201906290@g.uporto.pt>
2023-04-11 16:30:00 +0100

    Position class

---
+++ src/main/python/com/hmi_test_system/Position/Leds.py
@@ -0,0 +1,13 @@
+class Position:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def getPos():
+        return {Position.x, Position.y}
+    
+    def setPos(x, y):
+        Position.x = x
+        Position.y = y
+
+    
