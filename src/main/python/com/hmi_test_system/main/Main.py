3c13ecb631e69532cb86b753ad0998decee35757
Flipardo <filipedasilvaloureiro@gmail.com>
2023-04-12 18:28:21 +0100

    Main

--- src/main/python/com/hmi_test_system/main/Main.py
+++ src/main/python/com/hmi_test_system/main/Main.py
@@ -21,12 +21,18 @@ n_controll = 3
 
 x_alarm = 10
 y_alarm = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160]
+colour_alarm1 = 'Yellow'
+colour_alarm2 = 'Green'
 
 x_buttons = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180]
 y_buttons = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180]
+colour_button1 = 'Red'
+colour_button2 = 'Green'
 
 x_controll = [10, 20, 30]
 y_controll = 20
+colour_controll1 = 'Red'
+colour_controll2 = 'Green'
 
 x_display = 0
 y_display = 0
@@ -43,14 +49,26 @@ if(M.callModel(name) is None):
     index = M.indexModel(name)
     
     if(index != -1):
+        
         for i in n_alarm:
-            M.model[int(index)].setLedsAlarm(Led('LA'+str(i), 2, x_alarm, y_alarm[i]))
+            led = Led('LA'+str(i), 2, x_alarm, y_alarm[i])
+            led.newColour(colour_alarm1)
+            led.newColour(colour_alarm2)
+            M.model[int(index)].setLedsAlarm(led)
 
         for i in n_controll:
-            M.model[int(index)].setLedsControll(Led('LC'+str(i), 2, x_controll[i], y_controll))
+            led = Led('LC'+str(i), 2, x_controll[i], y_controll)
+            led.newColour(colour_controll1)
+            led.newColour(colour_controll2)
+            M.model[int(index)].setLedsControll(led)
 
         for i in n_buttons*2:
-            M.model[int(index)].setLedsButtons(Led('LB'+str(i), 2, x_buttons[i], y_buttons[i] ))
+            led = Led('LB'+str(i), 1, x_buttons[i], y_buttons[i])
+            if(i%2):
+                led.newColour(colour_button1)
+            else:
+                led.newColour(colour_button2)
+            M.model[int(index)].setLedsButtons(led)
 
         for i in n_buttons:
             M.model[int(index)].setButtonsModel(Button('BM'+str(i), 0, 0))
