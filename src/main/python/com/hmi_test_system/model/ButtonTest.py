20bc055f7aa70c74e671f3766ce432888eee4729
Flipardo <filipedasilvaloureiro@gmail.com>
2023-04-12 12:52:18 +0100

    Model part

--- src/main/python/com/hmi_test_system/model/ButtonTest.py
+++ src/main/python/com/hmi_test_system/model/ButtonTest.py
@@ -1,17 +1,21 @@
 class ButtonTest:
-    def __init__(self, button, pressedDisplay, pressedSerialPort):
+    def __init__(self, button):
         self.button = button
-        self.pressedDisplay = pressedDisplay
-        self.pressedSerialPort = pressedSerialPort 
+        self.pressedDisplay = bool
+        self.pressedSerialPort = bool
         
     def setButton(self, button):
         self.button = button
 
-    def getName(self):
-        pass
-        
-    def testPressedPass(self):
-        pass
+    #valor detetado pelo CV ao premir o botão
+    def pressTest(self, pressCV):
+        self.pressedDisplay = pressCV
 
-    def testPressedResult(self):
-        pass
+    def getPressedResult(self):
+        return self.pressedDisplay
+        
+    def test(self):
+        if self.pressedDisplay == 1:
+            return 1
+        else: 
+            return 0
--- src/main/python/com/hmi_test_system/model/DisplayTest.py
+++ src/main/python/com/hmi_test_system/model/DisplayTest.py
@@ -1,12 +1,47 @@
 class DisplayTest:
-    def __init__(self, display, test1, test2, test3):
+    def __init__(self, display):
         self.display = display
-        self.test1 = test1  
-        self.test2 = test2  
-        self.test3 = test3  
+        self.RGB = 1            
+        self.Pixel = 1          
+        self.Characters = 1     
+
+    # RGB screen detetado por CV
+    def RGB_CV(self, RGB):
+        self.RGB = RGB
+
+    def getRGBResult(self):
+        return self.RGB
+       
+    def testRGB(self):
+        if(self.RGB == 'imagem de referência'):
+            return 1
+        else:
+            return 0
+        
+
+    # Pixel screen detetado por CV
+    def Pixel_CV(self, Pixel):
+        self.Pixel = Pixel
     
-    def testPass(self):
-        pass
+    def getPixelResult(self):
+        return self.Pixel
+
+    def testPixel(self):
+        if(self.Pixel == 'imagem de referência'):
+            return 1
+        else:
+            return 0
     
-    def getTestResult(self):
-        pass
+
+    # Characters screen detetado por CV
+    def Characters_CV(self, Characters):
+        self.Characters = Characters
+
+    def getCharactersResult(self):
+        return self.Characters
+            
+    def testCharacters(self):
+        if(self.Characters == 'imagem de referência'):
+            return 1
+        else:
+            return 0
