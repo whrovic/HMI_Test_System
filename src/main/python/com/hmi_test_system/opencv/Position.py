fdeab7346cf88002f3eb0e33edd97ad2ae24273a
Ricardo Silva <up201906290@g.uporto.pt>
2023-04-12 22:14:56 +0100

    Update Led

--- src/main/python/com/hmi_test_system/opencv/Position.py
+++ src/main/python/com/hmi_test_system/opencv/Position.py
@@ -3,11 +3,11 @@ class Position:
         self.x = x
         self.y = y
 
-    def getPos():
-        return {Position.x, Position.y}
+    def getPos(self):
+        return {self.x, self.y}
     
-    def setPos(x, y):
-        Position.x = x
-        Position.y = y
+    def setPos(self, x, y):
+        self.x = x
+        self.y = y
 
     
