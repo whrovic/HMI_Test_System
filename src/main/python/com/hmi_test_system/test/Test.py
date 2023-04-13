4e1a07c4ecaf39bc0dee089946232a187960c1ad
Renato <renatopjesus1818@gmail.com>
2023-04-13 20:52:21 +0100

    Folder name change 4

--- src/main/python/com/hmi_test_system/test/Test.py
+++ src/main/python/com/hmi_test_system/test/Test.py
@@ -1,5 +1,5 @@
 from hmi_test_system.data.Model import Model
-from hmi_test_system.test.SequenceTest import SequenceTest
+from hmi_test_system.data.SequenceTest import SequenceTest
 from hmi_test_system.modelTest.ModelTest import ModelTest
 from hmi_test_system.modelTest.LedTest import LedTest
 from hmi_test_system.modelTest.ButtonTest import ButtonTest
