ed94856da07864b9db5929049c0397676cf02159
Renato <renatopjesus1818@gmail.com>
2023-04-13 14:22:07 +0100

    define Test class

--- src/main/python/com/hmi_test_system/test/SequenceTest.py
+++ src/main/python/com/hmi_test_system/test/SequenceTest.py
@@ -1,3 +1,4 @@
+from Test import Test
 
 class SequenceTest:
     pass
---
+++ src/main/python/com/hmi_test_system/test/Test.py
@@ -0,0 +1,14 @@
+from hmi_test_system.model.Model import Model
+from hmi_test_system.test.SequenceTest import SequenceTest
+from hmi_test_system.model.ModelTest import ModelTest
+from hmi_test_system.model.LedTest import LedTest
+from hmi_test_system.model.ButtonTest import ButtonTest
+from hmi_test_system.model.DisplayTest import DisplayTest
+
+class Test:
+        
+    def testButtonSerialPort(buttonTest: ButtonTest):
+        pass
+        
+    
+    pass
