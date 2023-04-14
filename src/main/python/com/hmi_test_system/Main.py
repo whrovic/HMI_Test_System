05bc17a8358aeaebcbb51bc9280dd150c54787a6
Renato <renatopjesus1818@gmail.com>
2023-04-14 18:11:04 +0100

    main change folders

--- src/main/python/com/hmi_test_system/Main.py
+++ src/main/python/com/hmi_test_system/Main.py
@@ -1,5 +1,6 @@
-import sys
-sys.path.append('c:/Users/filip/Desktop/ES/HMI_Test_System/src/main/python/com/hmi_test_system')
+#import sys
+#sys.path.append('c:/Users/filip/Desktop/ES/HMI_Test_System/src/main/python/com/hmi_test_system')
+#sys.path.append('c:/Users/asus/ES/HMI_Test_System/src/main/python/com/hmi_test_system')
 
 from data.Settings import Settings
 from Library import create_model
@@ -8,6 +9,7 @@ M = Settings()
 
 print("Nome do modelo: ")
 name_model = input()
+#Test File = "../data/m1.txt"
 
 if(M.call_model(name_model) is None):
     print("MODELO NOVO")
