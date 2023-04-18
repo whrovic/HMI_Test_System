46f3bcba057b680c2422f272d41c7487d766672d
Flipardo <filipedasilvaloureiro@gmail.com>
2023-04-18 18:47:05 +0100

    Terminal updated

--- src/main/python/com/hmi_test_system/Main.py
+++ src/main/python/com/hmi_test_system/Main.py
@@ -4,6 +4,8 @@ import os
 
 M = Settings()
 
+
+#------------------------------------MENU------------------------------------#
 while(1):
     os.system('cls') 
     print("---------------Menu---------------\n\n")
@@ -12,29 +14,40 @@ while(1):
     print("\n\n----------------------------------\n")
     c = input()
 
+    # add model
     if (c== '1'):
         n = 1
+
+    # test model    
     elif (c== '2'):
         n = 2
+
+    # turn off the program    
     elif(c=='3'):
         os.system('cls')
         break
+
     else:
         continue
+
+
 
 
     #------------------------------------ADD NEW MODEL------------------------------------#
     while(n==1):
         os.system('cls') 
         print("Insert the name of the new model:" )
-        print("(to go to the menu insert b)\n" )
+        print("(to go to the menu insert q)\n" )
         name_model = input()
         
-        if(name_model == 'b'):
+        # back to menu
+        if(name_model == 'q'):
             break
+
+        # model doesn't exist -> new configuration
         elif(M.call_model(name_model) is None):
             os.system('cls') 
-            print(f"                            {name_model} DOESN'T EXIST\n")
+            print(f"{name_model} DOESN'T EXIST\n")
             print("\n\n----------------------NEW MODEL CONFIGURATION----------------------\n")
 
             create_model(M, name_model)
@@ -44,6 +57,8 @@ while(1):
             print("To go to the menu insert anything\n")
             c = input()
             break
+
+        # model already exists
         else:
             os.system('cls') 
             print("MODEL ALREADY EXISTS\n\n")
@@ -58,68 +73,229 @@ while(1):
     while(n==2):
         os.system('cls') 
         print('What model do you want to test?')
-        print("(to go to the menu insert b)\n" )
+        print("(to go to the menu insert q)\n" )
         name_model = input()
         
-        if(name_model == 'b'):
+        # back to menu
+        if(name_model == 'q'):
             break
+        
+        # model doesn't exist
         elif(M.call_model(name_model) is None):
             os.system('cls') 
             print(f"{name_model} DOESN'T EXIST\n")
             print("To go to the menu insert anything\n")
             c = input()
             break
-
+        
+        # set model ready to test
         else:
             M.set_model_test(name_model)
             os.system('cls') 
-            print(f"MODEL {name_model} IS READY TO TEST\n")
+            print(f"{name_model} IS READY TO TEST\n")
             print("To start the test insert anything\n")
             c = input()
-            os.system('cls') 
-            print("-------------Test Menu--------------\n\n")
-            print("1- Led test         2- Button test\n")
-            print("3- LCD test         4- Menu")
-            print("\n\n----------------------------------\n")
-            c = input()
-            if (c== '1'):
-                n = 1
-            elif (c== '2'):
-                n = 2
-            elif (c== '3'):
-                n=3
-            elif(c=='4'):
-                break
-            else:
-                continue
 
-            #------------------------------------LED TEST------------------------------------#
-            while(n==1):
-                os.system('cls')
-                print("What led do you want to test?\n")
-                led_name = input()
-                index_led = M.index_led(led_name)
-                if(index_led is None):
-                    print(f"{led_name} DOESN'T EXIST")
-                else:
-                    pass
+            #------------------------------------TEST MENU------------------------------------#
+            while(1):
+                os.system('cls') 
+                print("------------Test Menu-------------\n\n")
+                print("1- Led test         2- Button test\n")
+                print("3- LCD test         4- Menu")
+                print("\n\n----------------------------------\n")
+                c = input()
                 
+                # led test
+                if (c== '1'):
+                    n2 = 1
 
-            #------------------------------------BUTTON TEST------------------------------------#
-            while(n==2):
-                os.system('cls')
-                print("What button do you want to test?\n")
-                button_name = input()
-                index_button = M.index_led(button_name)
-                if(index_button is None):
-                    print(f"{button_name} DOESN'T EXIST")
+                # button test  
+                elif (c== '2'):
+                    n2 = 2
+
+                # lcd test    
+                elif (c== '3'):
+                    n2 = 3
+
+                # back to menu
+                elif(c== '4'):
+                    n = 0
+                    break
+
                 else:
-                    pass
-                
-            #------------------------------------BUTTON TEST------------------------------------#
-            while(n==3):
-                pass
-            
+                    continue
+
+
+
+                #------------------------------------LED TEST------------------------------------#
+                while(n2==1):
+                    os.system('cls')
+                    print("What led do you want to test?\n")
+                    print("(to go to the menu insert q)\n" )
+                    led_name = input()
+
+                    # back to test menu
+                    if(led_name == 'q'):
+                        break
+
+                    # test a specific led
+                    else:
+                        index_led = M.index_led(led_name)
+
+                        # led doesn't exist
+                        if(index_led is None):
+                            print(f"{led_name} DOESN'T EXIST\n")
+                            print("To test another one or go to the test menu insert anything\n")
+                            c = input()
+                            continue
+                        
+                        # led exists
+                        else:
+                            result_test = 'FUNCAO'
+
+                            # test sucess
+                            if(result_test == -1):
+                                os.system('cls')
+                                print(f"{led_name} TEST FAILED\n")
+                                print("To test another one or go to the test menu insert anything\n")
+                                c = input()
+                                continue
+
+                            # test failed
+                            else:
+                                os.system('cls')
+                                print(f"{led_name} IS TESTED\n")
+                                print("To test another one or go to the test menu insert anything\n")
+                                c = input()
+                                continue
+                    
+                    
+
+                #------------------------------------BUTTON TEST------------------------------------#
+                while(n2==2):
+                    os.system('cls')
+                    print("What button do you want to test?\n")
+                    print("(to go to the test menu insert q)\n" )
+                    button_name = input()
+                    
+                    # back to test menu
+                    if(button_name == 'q'):
+                        break
+                    
+                    # test a specific button
+                    else:
+                        index_button = M.index_led(button_name)
+
+                         # button doesn't exist
+                        if(index_button is None):
+                            os.system('cls')
+                            print(f"{button_name} DOESN'T EXIST\n")
+                            print("To test another one or go to the test menu insert anything\n")
+                            c = input()
+                            continue
+                        
+                        # button exists
+                        else:
+                            result_test = 'FUNCAO'
+                            
+                            # test sucess
+                            if(result_test == -1):
+                                os.system('cls')
+                                print(f"{button_name} TEST FAILED\n")
+                                print("To test another one or go to the test menu insert anything\n")
+                                c = input()
+                                continue
+                            
+                            # test failed
+                            else:
+                                os.system('cls')
+                                print(f"{button_name} IS TESTED\n")
+                                print("To test another one or go to the test menu insert anything\n")
+                                c = input()
+                                continue
+
+
+
+                #------------------------------------LCD TEST------------------------------------#
+                while(n2==3):
+                    os.system('cls')
+                    print("-----------LCD Test Menu-----------\n\n")
+                    print("1- RGB test         2- Pixel test\n")
+                    print("3- Char test        4- Test menu")
+                    print("\n\n-----------------------------------\n")
+                    c = input()
+
+                    # RGB test
+                    if (c== '1'):
+                        result_test = 'FUNCAO'
+
+                        # test sucess
+                        if(result_test == -1):
+                            os.system('cls')
+                            print("RGB TEST FAILED\n")
+                            print("To test another thing or go to the test menu insert anything\n")
+                            c = input()
+                            continue
+
+                        # test failed
+                        else:
+                            os.system('cls')
+                            print("RGB TEST IS DONE\n")
+                            print("To test another thing or go to the test menu insert anything\n")
+                            c = input()
+                            continue
+
+                    # Pixel test
+                    elif (c== '2'):
+                        result_test = 'FUNCAO'
+
+                        # test sucess
+                        if(result_test == -1):
+                            os.system('cls')
+                            print("PIXEL TEST FAILED\n")
+                            print("To test another thing or go to the test menu insert anything\n")
+                            c = input()
+                            continue
+
+                        # test failed
+                        else:
+                            os.system('cls')
+                            print("PIXEL TEST IS DONE\n")
+                            print("To test another thing or go to the test menu insert anything\n")
+                            c = input()
+                            continue
+
+                    # Char test
+                    elif (c== '3'):
+                        result_test = 'FUNCAO'
+
+                        # test sucess
+                        if(result_test == -1):
+                            os.system('cls')
+                            print("CHAR TEST FAILED\n")
+                            print("To test another thing or go to the test menu insert anything\n")
+                            c = input()
+                            continue
+
+                        # test failed
+                        else:
+                            os.system('cls')
+                            print("CHAR TEST IS DONE\n")
+                            print("To test another thing or go to the test menu insert anything\n")
+                            c = input()
+                            continue
+
+                    # Back to test menu
+                    elif(c== '4'):
+                        n2 = 0
+                        break
+
+                    else:
+                        continue
+
+                    
+
+
 
 
             
