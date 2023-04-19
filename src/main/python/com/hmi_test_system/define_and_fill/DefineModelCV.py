555aa0122391be6d8b7651758e7cd5c6d67a0657
Pedro Duarte <56974612+whrovic@users.noreply.github.com>
2023-04-19 17:45:00 +0100

    Add detect_pos_display()

--- src/main/python/com/hmi_test_system/define_and_fill/DefineModelCV.py
+++ src/main/python/com/hmi_test_system/define_and_fill/DefineModelCV.py
@@ -48,6 +48,29 @@ class DefineModelCV():
         cv2.imshow("HMI", part)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
+    
+    @staticmethod
+    def detect_pos_display(image):
+        # Convert image to grayscale
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        # Convert to binary
+        binary = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)[1]
+
+        # Find contours
+        contours = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
+        
+        # Get LCD position
+        lcd = None
+        for contour in contours:
+            if cv2.contourArea(contour) > 50000 and cv2.contourArea(contour) < 336700:
+                x, y, w, h = cv2.boundingRect(contour)
+        if not x or  not y or not w or not h:
+            print("\nError: could not find LCD\n")
+            return None
+        
+        # Return LCD position
+        return x, y, w, h
 
 if(__name__ == "__main__"):
     image = cv2.imread("HMI.png")
