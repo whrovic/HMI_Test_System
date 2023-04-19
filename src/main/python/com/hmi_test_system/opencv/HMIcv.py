a938c484f4f0bc0d05abc900b75dd6cb12d29a20
Mariana <up201905543@fe.up.pt>
2023-04-19 22:57:44 +0100

    Add read_led_color()

--- src/main/python/com/hmi_test_system/opencv/HMIcv.py
+++ src/main/python/com/hmi_test_system/opencv/HMIcv.py
@@ -1,6 +1,16 @@
+import numpy as np
+from LEDcv import LEDcv
+
 class HMIcv():
+
     def led_test(img, leds, expected):
-        return False
+        #get the colors read on the led imagd
+        led_colors = LEDcv.read_led_color(img, leds)
+        #compare the colors obtained with the expected ones
+        if np.array_equal(led_colors, expected):
+            return True
+        else:
+            return False
     
     def display_characters_test(img, lcd):
         return False
