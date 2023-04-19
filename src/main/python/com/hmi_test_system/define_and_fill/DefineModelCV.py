3f8692cacba8c5acc51125630a1d35990a137b4c
Ricardo Silva <up201906290@g.uporto.pt>
2023-04-19 17:35:34 +0100

    Define Poistion LED

--- src/main/python/com/hmi_test_system/define_and_fill/DefineModelCV.py
+++ src/main/python/com/hmi_test_system/define_and_fill/DefineModelCV.py
@@ -1,37 +1,54 @@
 import cv2
+import numpy as np
 
 class DefineModelCV():
-    
-    @staticmethod
-    def detectPosLed(image):
 
-        coordenadas = []
+    @staticmethod
+    def definePosLed(image):
+
+        print('Click in the LED and press ENTER')
+        coordenadas = DefineModelCV.clickPosLed(image)
+        print('Check the position and press ENTER')
+        DefineModelCV.printPosLed(image, coordenadas)
+
+        while (input('Is that the correct position? [Y/N]') != 'Y'):
+            print('Click in the LED and press ENTER')
+            coordenadas = DefineModelCV.clickPosLed(image)
+            print('Check the position and press ENTER')
+            DefineModelCV.printPosLed(image, coordenadas)
+
+        print('Fim')
+
+    @staticmethod
+    def clickPosLed(image):
+
+        coordenadas = [0, 0]
 
         def callback(event, x, y, flags, params):
             if(event == cv2.EVENT_LBUTTONDOWN):
-                coordenadas.append(x)
-                coordenadas.append(y)
+                coordenadas[0] = x
+                coordenadas[1] = y
                 
         cv2.imshow("HMI", image)
-        cv2.setMouseCallback('HMI', callback)
+        cv2.setMouseCallback("HMI", callback)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
         return coordenadas
     
     @staticmethod
-    def printLed(image, coordenadas):
-        for i in range(coordenadas[1]-2,coordenadas[1]+2):
-            for j in range(coordenadas[0]-2,coordenadas[0]+2):
-                image[i][j] = (0,0,0)
+    def printPosLed(image, coordenadas):
 
-        cv2.imshow("HMI", image)
+        part = np.copy(image)
+
+        for i in range(coordenadas[1] - 2, coordenadas[1] + 2):
+            for j in range(coordenadas[0] - 2, coordenadas[0] + 2):
+                part[i][j] = (0,0,0)
+
+        cv2.imshow("HMI", part)
         cv2.waitKey(0)
-        
-image = cv2.imread("HMI.png")
+        cv2.destroyAllWindows()
 
-coordenadas = DefineModelCV.detectPosLed(image)
-
-print(coordenadas)
-
-DefineModelCV.printLed(image, coordenadas)
+if(__name__ == "__main__"):
+    image = cv2.imread("HMI.png")
+    coordenadas = DefineModelCV.definePosLed(image)
--- src/main/python/com/hmi_test_system/opencv/LEDcv.py
+++ src/main/python/com/hmi_test_system/opencv/LEDcv.py
@@ -1,8 +1,6 @@
-import cv2
-
 class LEDcv:
     def cut_led(image, led):
-        return image[led.get_pos_x-2:led.get_pos_x+2, led.get_pos_y-2:led.get_pos_y+2]
+        return image[led.get_pos_y-2:led.get_pos_y+2, led.get_pos_x-2:led.get_pos_x+2]
 
 
 
