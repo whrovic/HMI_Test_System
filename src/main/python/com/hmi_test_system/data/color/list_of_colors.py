3b96d8414b30564c66084c70d1291f380c19c422
José Francisco Ribeiro Carvalho <up201906240@edu.fe.up.pt>
2023-04-21 21:00:33 +0100

    list_of_colors template

--- src/main/python/com/hmi_test_system/data/color/list_of_colors.py
+++ src/main/python/com/hmi_test_system/data/color/list_of_colors.py
@@ -14,4 +14,44 @@ A prioridade é integrar estas novas classes com as funções de teste dos leds.
 '''
 
 class ListOfColors:
-    pass
+    
+    _list_of_colors = []
+
+    @staticmethod
+    def get_list_of_colors():
+        return ListOfColors._list_of_colors
+
+    '''
+    Deve percorrer o vetor e retornar a cor com o nome indicado (ou None se nao encontrar)
+    '''
+    @staticmethod
+    def get_color(name):
+        return None
+
+    '''
+    Deve verificar se a cor existe. Se não existir adiciona uma nova cor
+    '''
+    @staticmethod
+    def add_color(name, hsv_min1, hsv_max1, hsv_min2=None, hsv_max2=None):
+        pass
+    
+    '''
+    Remove a cor com este nome
+    '''
+    @staticmethod
+    def remove_color(name):
+        pass
+
+    '''
+    Guarda o vetor de cores, todas as cores e as propriedades para um ficheiro (.txt, .xml, .json, whatever)
+    '''
+    @staticmethod
+    def save_to_file(filename):
+        pass
+
+    '''
+    Lê o ficheiro e recupera o vetor com todas as cores
+    '''
+    @staticmethod
+    def read_from_file(filename):
+        pass
