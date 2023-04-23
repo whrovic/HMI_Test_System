6ca13645da2046767b9bac739184bb543a0f802d
Sanches <mjlsanches2001@gmail.com>
2023-04-23 16:51:46 +0100

    Some minor corrections

--- src/main/python/com/hmi_test_system/model_test/LedTest.py
+++ src/main/python/com/hmi_test_system/model_test/LedTest.py
@@ -1,25 +1,28 @@
+import string
+
 from data.model.Led import Led
 
 
 class LedTest:
     def __init__(self, led: Led):
         self.led = led
-        self.colours_cv = []
-    
+        self.colours_cv: list[string] = []
+
     def set_led(self, led):
         self.led = led
 
-    #vetor de cores detetas por cv
-    def colour_test(self, colour):
+    # vetor de cores detetas por cv
+    def test_colour(self, colour):
         self.colours_cv.append(colour)
 
     def get_colour_result(self):
         return self.colours_cv
-    
-    def test_Led(self):
-        if(self.colours_cv == self.led._colours):
+
+    def result_test_Led(self):
+        if self.colours_cv == self.led._colours:
             return 1
         else:
             return 0
 
-
+    def clear_colour(self):
+        self.colours_cv.clear()
--- src/main/python/com/hmi_test_system/model_test/ModelTest.py
+++ src/main/python/com/hmi_test_system/model_test/ModelTest.py
@@ -1,9 +1,19 @@
-from model_test.DisplayTest import DisplayTest
-from model_test.ButtonTest import ButtonTest
-from model_test.LedTest import LedTest
+from .DisplayTest import DisplayTest
+from .ButtonTest import ButtonTest
+from .LedTest import LedTest
+
 
 class ModelTest:
     def __init__(self):
         self.leds_test: list[LedTest] = []
         self.buttons_test: list[ButtonTest] = []
         self.display_test: DisplayTest
+
+    def clear_model_test(self):
+        for i in range(0, len(self.buttons_test)):
+            self.buttons_test[i].test_press_serial_port(False)
+            self.buttons_test[i].test_press_display(False)
+
+        for i in range(0, len(self.leds_test)):
+            self.leds_test[i].clear_colour()
+
