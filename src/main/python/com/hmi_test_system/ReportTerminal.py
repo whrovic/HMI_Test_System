811b052e4f4f389a484b04d0a9ded4c453d4aec9
khaled <up202202482@g.uporto.pt>
2023-04-24 15:09:37 +0100

    chanches report

--- src/main/python/com/hmi_test_system/ReportTerminal.py
+++ src/main/python/com/hmi_test_system/ReportTerminal.py
@@ -4,6 +4,11 @@ from model_test.ButtonTest import ButtonTest
 from model_test.LedTest import LedTest
 from model_test.DisplayTest import DisplayTest
 
+from data.Led import Led
+from data.Button import Button
+from data.Display import Display
+
+
 class ReportTerminal:
 
     def __init__(self, model_test : ModelTest):
@@ -18,14 +23,22 @@ class ReportTerminal:
 
     def LedTerminal(self):
         
+     Led.result_press_display()
+        
 
     def DisplayTerminal(self):    
+        
 
+#####################################################################
 
-mt = ModelTest()
+leds = [Led('L1','red',1,2), Led('L2','red',3,5), Led('L3','ok',5,7)]
+#buttons = [Button((2, 3)), Button((4,5)), Button((7,9))]
+display = Display('d1',5,7,10,20)
+
+mt = ModelTest(leds, display)
 
 report = ReportTerminal(mt)
 
-tt = LedTest()
+
 report.LedTerminal()
 
