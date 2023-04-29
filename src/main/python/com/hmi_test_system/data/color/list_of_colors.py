d267a3446157ca7a4d39b82a6177ce7d4997bb84
Mariana <up201905543@fe.up.pt>
2023-04-29 23:06:06 +0100

    remove comments and change in LEDcv

--- src/main/python/com/hmi_test_system/data/color/list_of_colors.py
+++ src/main/python/com/hmi_test_system/data/color/list_of_colors.py
@@ -1,17 +1,3 @@
-'''
-(Mariana)
-
-Esta classe deve guardar todas as classes que sejam necessárias. Não é para guardar OffColor e UnknownColor.
-
-As cores devem ser guardadas num ficheiro (.txt, .properties, whatever) e quando a classe é criada (no __init__),
-lê todas as cores e as propriedades desse ficheiro.
-
-Devem haver funções para criar nova cor ou remover.
-
-As funções de ler e escrever para o ficheiro deixa para último.
-Primeiro adiciona-se sempre manualmente com a função add_color todas as cores.
-A prioridade é integrar estas novas classes com as funções de teste dos leds.
-'''
 
 class ListOfColors:
     
@@ -42,10 +28,7 @@ class ListOfColors:
         ListOfColors._list_of_colors.append(new_color)
         print(f"Color {name} added successfully!")
     
-    '''
-    Remove a cor com este nome
-    Baixa prioridade
-    '''
+
     @staticmethod
     def remove_color(name):
         for color in ListOfColors._list_of_colors:
--- src/main/python/com/hmi_test_system/opencv/LEDcv.py
+++ src/main/python/com/hmi_test_system/opencv/LEDcv.py
@@ -1,7 +1,7 @@
 
 import cv2
 import numpy as np
-from data.model.Led import Led
+from data.model import Led
 
 class LEDcv:
     
@@ -32,52 +32,50 @@ class LEDcv:
     (Alta prioridade)
     '''
     def read_color(image):
-        return "deve retornar a cor da class Color"
+        color 
+        # Convert the image from BGR to HSV color space
+        hsv_img = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+        # Define the color ranges to detect
+        # (red is a more complex color to detect
+        # because it can vary significantly in hue 
+        # depending on the lighting conditions so it
+        # has 4 ranges)
+        lower_red1 = np.array([0, 50, 50])
+        upper_red1 = np.array([10, 255, 255])
+        lower_red2 = np.array([170, 50, 50])
+        upper_red2 = np.array([180, 255, 255])
+        lower_yellow = np.array([20, 50, 50])
+        upper_yellow = np.array([45, 255, 255])
+        lower_green = np.array([60, 50, 50])
+        upper_green = np.array([90, 255, 255])
+
+        # Threshold the image to isolate the dominant color
+        mask_red1 = cv2.inRange(hsv_img, lower_red1, upper_red1)
+        mask_red2 = cv2.inRange(hsv_img, lower_red2, upper_red2)
+        mask_red = cv2.bitwise_or(mask_red1, mask_red2)
+        mask_yellow = cv2.inRange(hsv_img, lower_yellow, upper_yellow)
+        mask_green = cv2.inRange(hsv_img, lower_green, upper_green)
+
+        # Calculate the number of pixels for each color
+        total_pixels = hsv_img.shape[0] * hsv_img.shape[1]
+        red_pixels = cv2.countNonZero(mask_red)
+        yellow_pixels = cv2.countNonZero(mask_yellow)
+        green_pixels = cv2.countNonZero(mask_green)
+
+        # Calculate the percentage of pixels for each color
+        red_percentage = (red_pixels / total_pixels) * 100
+        yellow_percentage = (yellow_pixels / total_pixels) * 100
+        green_percentage = (green_pixels / total_pixels) * 100
+
+        # Determine the dominant color
+        if red_percentage > yellow_percentage and red_percentage > green_percentage:
+            color = "Red"
+        elif yellow_percentage > red_percentage and yellow_percentage > green_percentage:
+            color = "Yellow"
+        elif green_percentage > red_percentage and green_percentage > yellow_percentage:
+            color = "Green"
+        else:
+            color = "Unknown"
     
-        colors = []
-
-        for led in ledTest:
-            img = LEDcv.cut_led(image, led)
-
-            # Convert the image from BGR to HSV color space
-            hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
-            # Define the color ranges to detect
-            # (red is a more complex color to detect
-            # because it can vary significantly in hue 
-            # depending on the lighting conditions so it
-            # has 4 ranges)
-            lower_red1 = np.array([0, 50, 50])
-            upper_red1 = np.array([10, 255, 255])
-            lower_red2 = np.array([170, 50, 50])
-            upper_red2 = np.array([180, 255, 255])
-            lower_yellow = np.array([20, 50, 50])
-            upper_yellow = np.array([45, 255, 255])
-            lower_green = np.array([60, 50, 50])
-            upper_green = np.array([90, 255, 255])
-
-            # Threshold the image to isolate the dominant color
-            mask_red1 = cv2.inRange(hsv_img, lower_red1, upper_red1)
-            mask_red2 = cv2.inRange(hsv_img, lower_red2, upper_red2)
-            mask_red = cv2.bitwise_or(mask_red1, mask_red2)
-            mask_yellow = cv2.inRange(hsv_img, lower_yellow, upper_yellow)
-            mask_green = cv2.inRange(hsv_img, lower_green, upper_green)
-
-            # Calculate the number of pixels for each color
-            red_pixels = cv2.countNonZero(mask_red)
-            yellow_pixels = cv2.countNonZero(mask_yellow)
-            green_pixels = cv2.countNonZero(mask_green)
-
-            for i in colors:
-
-                # Determine the dominant color
-                if red_pixels > yellow_pixels and red_pixels > green_pixels:
-                    colors[i] = "Red"
-                elif yellow_pixels > red_pixels and yellow_pixels > green_pixels:
-                    colors[i] = "Yellow"
-                elif green_pixels > red_pixels and green_pixels > yellow_pixels:
-                    colors[i] = "Green"
-                else:
-                    colors[i] = "Unknown"
-        
-        return colors
+        return color
