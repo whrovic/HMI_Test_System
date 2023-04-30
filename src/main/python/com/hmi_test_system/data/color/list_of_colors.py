db9305441633d7a3cc3c64c8e892eab7e8d8342a
Mariana <up201905543@fe.up.pt>
2023-04-30 11:10:37 +0100

    Read_color function

--- src/main/python/com/hmi_test_system/data/color/list_of_colors.py
+++ src/main/python/com/hmi_test_system/data/color/list_of_colors.py
@@ -1,21 +1,9 @@
-'''
-(Mariana)
-
-Esta classe deve guardar todas as classes que sejam necessárias. Não é para guardar OffColor e UnknownColor.
-
-As cores devem ser guardadas num ficheiro (.txt, .properties, whatever) e quando a classe é criada (no __init__),
-lê todas as cores e as propriedades desse ficheiro.
-
-Devem haver funções para criar nova cor ou remover.
-
-As funções de ler e escrever para o ficheiro deixa para último.
-Primeiro adiciona-se sempre manualmente com a função add_color todas as cores.
-A prioridade é integrar estas novas classes com as funções de teste dos leds.
-'''
+from .color import Color
+from typing import List
 
 class ListOfColors:
     
-    _list_of_colors: list[Color]= []
+    _list_of_colors: List[Color]= []
 
     @staticmethod
     def get_n_colors():
