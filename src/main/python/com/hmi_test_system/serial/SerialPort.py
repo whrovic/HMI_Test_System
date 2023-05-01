a8d04f6c093bad2da2b05d1911d870b950ca91f5
Ricardo Silva <up201906290@g.uporto.pt>
2023-05-01 12:51:59 +0100

    Creation of SerialPort class attributes

--- src/main/python/com/hmi_test_system/serial/SerialPort.py
+++ src/main/python/com/hmi_test_system/serial/SerialPort.py
@@ -1,3 +1,30 @@
+from threading import Thread
+import queue
 
-class SerialPort:      
-    pass
+class SerialPort:
+
+    thread : Thread
+    is_receiving : bool
+    
+    def __init__(self):
+        self.is_receiving = False
+        self.thread = Thread(target = self.thread_loop)
+        self.port_queue = queue.Queue()
+
+    def start_receive(self):
+        pass
+
+    def stop_receive(self):
+        pass
+
+    def thread_loop(self):
+        pass
+
+    def read_port(self):
+        pass
+
+    def clear_queue(self):
+        pass
+
+
+    
