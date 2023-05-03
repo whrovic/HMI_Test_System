6beae1ca169a6847abe58a8a6bcbb4923b2c1afd
Ricardo Silva <up201906290@g.uporto.pt>
2023-05-03 14:04:43 +0100

    Creation of SerialPort class methods: start_capture e stop_capture

--- src/main/python/com/hmi_test_system/serial/SerialPort.py
+++ src/main/python/com/hmi_test_system/serial/SerialPort.py
@@ -4,8 +4,7 @@ import queue
 class SerialPort:
 
     is_receiving : bool
-    thread : Thread
-    port_queue : queue    
+    thread : Thread 
     
     def __init__(self):
         self.is_receiving = False
@@ -13,19 +12,21 @@ class SerialPort:
         self.port_queue = queue.Queue()
 
     def start_receive(self):
-        pass
+        self.is_receiving = True
+        self.thread.start()
 
     def stop_receive(self):
-        pass
+        self.is_receiving = False
+        self.thread.join()
 
     def thread_loop(self):
-        pass
+        while self.is_receiving:
 
     def read_port(self):
         pass
 
     def clear_queue(self):
-        pass
+        self.port_queue.queue.clear()
 
 
     
