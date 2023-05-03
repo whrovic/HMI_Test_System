7fe9291e5fadea52ff8e53703210c6f844a2dada
José Francisco Ribeiro Carvalho <up201906240@edu.fe.up.pt>
2023-05-03 15:29:48 +0100

    path is now global

--- src/main/python/com/hmi_test_system/data/path.py
+++ src/main/python/com/hmi_test_system/data/path.py
@@ -1,19 +1,42 @@
 import os
 
-class path:
+class Path:
     def __init__(self):
-        self.xml_directory: str
-        self.report_directory: str
+        file_path = os.path.dirname(__file__)
+
+        Path.xml_directory = os.path.join(file_path, '../../../../../../xml_files')
+        Path.report_directory = os.path.join(file_path, '../../../../../../reports')
+        Path.resources_directory = os.path.join(file_path, '../../../../resources')
+        Path.settings_directory = os.path.join(file_path, '../../../../../../settings')
+
+    @staticmethod
+    def set_settings_directory(settings_directory):
+        Path.settings_directory = settings_directory
+
+    @staticmethod
+    def get_settings_directory():
+        return Path.settings_directory
+
+    @staticmethod
+    def set_resources_directory(resources_directory):
+        Path.resources_directory = resources_directory
+
+    @staticmethod
+    def get_resources_directory():
+        return Path.resources_directory
+
+    @staticmethod
+    def set_xml_direcory(xml_direcory):
+        Path.xml_directory = xml_direcory
     
-    def set_xml_direcory(self, xml_direcory):
-        self.xml_directory = xml_direcory
-        #if os.path.exists(xml_direcory):
-        
-    def get_xml_directory(self):
-        return self.xml_directory
+    @staticmethod
+    def get_xml_directory():
+        return Path.xml_directory
     
-    def set_report_direcory(self, report_direcory):
-        self.report_directory = report_direcory
+    @staticmethod
+    def set_report_direcory(report_direcory):
+        Path.report_directory = report_direcory
       
-    def get_report_directory(self):
-        return self.report_directory
+    @staticmethod
+    def get_report_directory():
+        return Path.report_directory
