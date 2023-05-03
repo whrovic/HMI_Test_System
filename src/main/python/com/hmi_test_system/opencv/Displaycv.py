f5605f4d510eb1066c82d7eec5bc190ba78d49e3
Pedro Duarte <56974612+whrovic@users.noreply.github.com>
2023-05-03 15:14:02 +0100

    Update Displaycv.py

--- src/main/python/com/hmi_test_system/opencv/Displaycv.py
+++ src/main/python/com/hmi_test_system/opencv/Displaycv.py
@@ -1,47 +1,45 @@
 import cv2, pytesseract
 from data.model.Display import Display
 
-class Displaycv:
+class Displaycv():
     
     '''
     (Pedro)
     '''
     @staticmethod
-    def read_char(lcd):
+    def read_char(display):
         # Setup tesseract
         pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-        # Convert LCD image to grayscale
-        gray = cv2.cvtColor(lcd, cv2.COLOR_BGR2GRAY)
+        # Convert display image to grayscale
+        gray = cv2.cvtColor(display, cv2.COLOR_BGR2GRAY)
 
         # Convert to binary with adaptive threshold
         binary = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 37, 35)
         
-        # Read characters on LCD
-        char_pattern = pytesseract.image_to_string(binary, lang='eng', config='--psm 6') # psm 6 --> Assume a single uniform block of text
-        if char_pattern is None:
-            return ""
+        # Read characters on display
+        character_pattern = pytesseract.image_to_string(binary, lang='eng', config='--psm 6') # psm 6 --> Assume a single uniform block of text
         
-        # Remove unnecessary whitespaces and new lines
-        char_pattern = char_pattern.replace(" ", "").replace("\n\n", "\n")
+        # Remove unnecessary whitespaces and duplicate newlines
+        character_pattern = character_pattern.replace(" ", "").replace("\n\n", "\n")
         
-        # Return characters pattern
-        return char_pattern
+        # Return character pattern
+        return character_pattern
     
     '''
     (Pedro)
     '''
     @staticmethod
-    def get_color_pattern(lcd):
-        # Resize LCD image
-        lcd = cv2.resize(lcd, (680, 512))
+    def get_color_pattern(display):
+        # Resize display image
+        display = cv2.resize(display, (680, 512))
 
         # Calculate the width and height of each rectangle
-        width = lcd.shape[1] // 40
-        height = lcd.shape[0] // 16
+        width = display.shape[1] // 40
+        height = display.shape[0] // 16
 
         # Create a list to store the colors
-        colors = []
+        color_pattern = []
         # Loop over each rectangle and extract the color
         for i in range(40):
             for j in range(16):
@@ -49,17 +47,20 @@ class Displaycv:
                 x = i * width
                 y = j * height
                 # Extract the color of the rectangle by taking the average color value
-                color = cv2.mean(lcd[y:y+height, x:x+width])[:3]
+                color = cv2.mean(display[y:y+height, x:x+width])
                 # Add the color to the list of colors
-                colors.append(color)
+                color_pattern.append(color)
 
-        # Return the color pattern
-        return colors
+        # Return color pattern
+        return color_pattern
     
     '''
     (Pedro)
     '''
     @staticmethod
-    def cut_lcd(img, lcd: Display):
-        x, y, w, h = lcd.get_pos_x(), lcd.get_pos_y(), lcd.get_dim_x(), lcd.get_dim_y()
-        return img[y:y+h, x:x+w]
+    def cut_display(image, display: Display):
+        # Get display position
+        x, y, w, h = display.get_pos_x(), display.get_pos_y(), display.get_dim_x(), display.get_dim_y()
+
+        # Return display
+        return image[y:y+h, x:x+w]
