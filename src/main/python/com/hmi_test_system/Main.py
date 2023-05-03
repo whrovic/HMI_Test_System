188feb5be48091fcd52f05c7bebe4f2bd71b705a
Flipardo <filipedasilvaloureiro@gmail.com>
2023-05-03 16:28:00 +0100

    ta

--- src/main/python/com/hmi_test_system/Main.py
+++ src/main/python/com/hmi_test_system/Main.py
@@ -25,16 +25,6 @@ ListOfColors.read_from_file(M.path.get_settings_directory() + "/colors.json")
 #ListOfColors.add_color("Yellow", [20, 50, 50], [45, 255, 255])
 #ListOfColors.add_color("Green", [60, 50, 50], [90, 255, 255])
 
-# directory where you want to save your model files
-#xml_directory = "C:/Users/filip/Desktop/ES/HMI_Test_System/xml_files"
-#xml_directory = "C:/Users/asus/ES/HMI_Test_System/xml_files"
-#xml_directory = "C:/Users/Drago/OneDrive/Ambiente de Trabalho/Escola/4Ano/2S/ES - Engenharia de Sistemas/2. Projeto/HMI_Test_System/xml_files"
-#xml_directory = "C:/Users/prica/FEUP/ES/HMI_Test_System/xml_files"
-
-# directory where you want to save your reports
-#report_directory = "C:/Users/filip/Desktop/ES/HMI_Test_System/reports"
-#report_directory = "C:/Users/asus/ES/HMI_Test_System/reports"
-#report_directory = "C:/Users/Drago/OneDrive/Ambiente de Trabalho/Escola/4Ano/2S/ES - Engenharia de Sistemas/2. Projeto/HMI_Test_System/reports"
 
 
 def new_model(M: Settings):
