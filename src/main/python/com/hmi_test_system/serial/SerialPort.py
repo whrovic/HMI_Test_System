7566de1390e68f71d618647bf5392d11fcfc13f3
Ricardo Silva <up201906290@g.uporto.pt>
2023-05-03 14:57:08 +0100

    Completing of SerialPort class methods

--- src/main/python/com/hmi_test_system/serial/SerialPort.py
+++ src/main/python/com/hmi_test_system/serial/SerialPort.py
@@ -1,15 +1,17 @@
 from threading import Thread
+import serial
 import queue
 
 class SerialPort:
 
     is_receiving : bool
-    thread : Thread 
+    thread : Thread
     
     def __init__(self):
         self.is_receiving = False
         self.thread = Thread(target = self.thread_loop)
         self.port_queue = queue.Queue()
+        self.serial = serial.Serial(port = 'COM1', baudrate = 115200, bytesize = 8, parity = serial.PARITY_NONE, stopbits = 1, flowcontrol = False)
 
     def start_receive(self):
         self.is_receiving = True
@@ -21,9 +23,11 @@ class SerialPort:
 
     def thread_loop(self):
         while self.is_receiving:
+            self.read_port()
 
     def read_port(self):
-        pass
+        if self.serial.in_waiting() != 0:
+            self.port_queue.put(self.serial.readline().decode())
 
     def clear_queue(self):
         self.port_queue.queue.clear()
