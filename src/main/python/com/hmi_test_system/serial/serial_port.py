6a67c8bc1754f30fc019751852faf1a24c7d4960
José Francisco Ribeiro Carvalho <up201906240@edu.fe.up.pt>
2023-05-08 18:06:35 +0100

    serial port small changes

--- src/main/python/com/hmi_test_system/serial/serial_port.py
+++ src/main/python/com/hmi_test_system/serial/serial_port.py
@@ -7,11 +7,15 @@ class SerialPort:
     is_receiving : bool
     thread : Thread
     
-    def __init__(self):
+    def __init__(self, port):
         self.is_receiving = False
         self.thread = Thread(target = self.thread_loop)
         self.port_queue = queue.Queue()
-        self.serial = serial.Serial(port = 'COM1', baudrate = 115200, bytesize = 8, parity = serial.PARITY_NONE, stopbits = 1, flowcontrol = False)
+        self.serial = serial.Serial(port = port, baudrate = 115200, bytesize = 8, parity = serial.PARITY_NONE, stopbits = 1, xonxoff=False)
+
+    # igual ao que tem no get_image
+    def get_serial(self):
+        pass
 
     def start_receive(self):
         self.is_receiving = True
@@ -28,6 +32,10 @@ class SerialPort:
     def read_port(self):
         if self.serial.in_waiting() != 0:
             self.port_queue.put(self.serial.readline().decode())
+
+    # enviar uma string por serial port
+    def write_port(self, data):
+        pass
 
     def clear_queue(self):
         self.port_queue.queue.clear()
