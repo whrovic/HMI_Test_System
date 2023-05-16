4d851e250daabdd3b65ff7713364efbb697ec738
Sanches <mjlsanches2001@gmail.com>
2023-05-16 16:04:02 +0100

    continuation of the state machine in test.py

--- src/main/python/com/hmi_test_system/test/test.py
+++ src/main/python/com/hmi_test_system/test/test.py
@@ -4,6 +4,7 @@ from tkinter import Image
 from ..data.model.model import Model
 from ..model_test.model_test import ModelTest
 from ..model_test.led_test import LedTest
+from ..data.model.led import Led
 from ..model_test.button_test import ButtonTest
 from ..model_test.display_test import DisplayTest
 from ..opencv.hmicv import HMIcv
@@ -11,12 +12,12 @@ from ..video.camera import Camera
 from ..data.color.color import Color
 
 cam_value: Camera
-vet_cor: list [37]
-vet_cor_bef: list [37]
+vet_cor: list[37]
+vet_cor_bef: list[56][37]
 
 
-#TODO: complet the start_test and end_test 
-# whith the right functions
+# TODO: complet the start_test and end_test
+# with the right functions
 
 class Test:
 
@@ -53,47 +54,66 @@ class Test:
         self.test_button_display()
         self.test_button_serial_port()
 
-    def test_led(self, leds_test: list[LedTest]):
+    def test_led(self, leds_test: list[Led]):
         state = 0
         aux = 0
         while 1:
             cam = cam_value.get_image()
+
             for i in range(0, len(leds_test)):
-                vet_cor[i] = HMIcv.led_test(cam, leds_test[i].get_led())
+                vet_cor[i] = HMIcv.led_test(cam, leds_test[i])
             if state == 0:
                 aux = 0
                 for i in range(0, len(leds_test)):
                     if vet_cor[i] is not None:
                         aux = aux + 1
                 if aux == len(leds_test):
+                    led_test_pass_terminal(state)
                     state = 1
                 else:
-                    # fazer funçao de escrever no terminal
+                    led_test_error_terminal(state)
                     return -1
+
             if state == 1:
                 aux = 0
                 for i in range(0, len(leds_test)):
                     if vet_cor[i] is None:
                         aux = aux + 1
                 if aux == len(leds_test):
+                    led_test_pass_terminal(state)
                     state = 2
                 else:
-                    # fazer funçao de escrever no terminal
+                    led_test_error_terminal(state)
                     return -1
+
             if state == 2:
                 aux = 0
                 for i in range(0, len(leds_test)):
                     if vet_cor[i] is not None:
                         aux = aux + 1
                 if aux == len(leds_test):
+                    led_test_pass_terminal(state)
                     state = 3
                 else:
-                    # fazer funçao de escrever no terminal
+                    led_test_error_terminal(state)
                     return -1
+
             if state == 3:
                 aux = 0
-                #for i in range(0, len(leds_test)):
-                if vet_cor[0].get_name() == 'green' and vet_cor[0].get_name() != vet_cor_bef[0].get_name():
+                cam_bef = None
+                for i in range(0, 56):
+                    for j in range(0, len(leds_test)):
+                        if cam != cam_bef:
+                            vet_cor_bef[i][j] = HMIcv.led_test(cam, leds_test[i])
+                    cam_bef = cam
+                    cam = cam_value.get_image()
+                for i in range(0, 56):
+                    for j in range(0, len(leds_test)):
+                        for k in range(0, len(leds_test[j].get_colour())):
+                            if vet_cor_bef[i][j] == leds_test[j].get_colour()[k]:
+                                aux = aux + 1
+                            elif vet_cor_bef[i][j] != leds_test[j].get_colour()[k]:
+
 
     def test_display(self, display: DisplayTest, test):
         if test == 1:
@@ -137,3 +157,17 @@ class Test:
                 return 0
             else:
                 return -1
+
+
+def led_test_error_terminal(code):
+    if code == 0 or code == 2:
+        print("Error on turned on LED's\n")
+    elif code == 1:
+        print("Error on turned off LED's\n")
+
+
+def led_test_pass_terminal(code):
+    if code == 0 or code == 2:
+        print("All the leds Turn ON")
+    elif code == 1:
+        print("All the leds Turn OFF")
