2b227d60cb8a12a5ffe6a0ff0b4dc3f67ce37707
Renato <renatopjesus1818@gmail.com>
2023-05-16 16:11:06 +0100

    updates

--- src/main/python/com/hmi_test_system/test/test.py
+++ src/main/python/com/hmi_test_system/test/test.py
@@ -3,19 +3,23 @@ from opencv.hmicv import HMIcv
 from video.camera import Camera
 from serial.serial_port import SerialPort
 import time
-
+from ..data.model.led import Led
+from ..data.model.button import Button
+from ..data.model.display import Display
 from ..data.model.model import Model
 from ..model_test.model_test import ModelTest
-from ..model_test.led_test import LedTest
-from ..model_test.button_test import ButtonTest
-from ..model_test.display_test import DisplayTest
+#from ..model_test.led_test import LedTest
+#from ..model_test.button_test import ButtonTest
+#from ..model_test.display_test import DisplayTest
 from ..opencv.hmicv import HMIcv
 from ..video.camera import Camera
 from ..data.color.color import Color
 
 cam_value: Camera
-vet_cor: [37]
-vet_cor_bef: [37]
+vet_cor: list[37]
+vet_cor_bef: list[56][37]
+
+
 
 
 # TODO: complet the start_test and end_test
@@ -162,7 +166,7 @@ class Test:
     def end_test():
         cam_value.stop_capture()
 
-    def test_button_display(self, buttons_test: list[ButtonTest]):
+    '''def test_button_display(self, buttons_test: list[ButtonTest]):
         # for i in range(0, len(buttons_test)):
         #     result =
         pass
@@ -173,51 +177,67 @@ class Test:
 
     def test_button(self, buttons_test: list[ButtonTest]):
         self.test_button_display()
-        self.test_button_serial_port()
+        self.test_button_serial_port()'''
 
-    def test_led(self, leds_test: list[LedTest]):
+    def test_led(self, leds_test: list[Led]):
         state = 0
         aux = 0
         while 1:
             cam = cam_value.get_image()
+
             for i in range(0, len(leds_test)):
-                vet_cor[i] = HMIcv.led_test(cam, leds_test[i].get_led())
+                vet_cor[i] = HMIcv.led_test(cam, leds_test[i])
             if state == 0:
                 aux = 0
                 for i in range(0, len(leds_test)):
                     if vet_cor[i] is not None:
                         aux = aux + 1
                 if aux == len(leds_test):
+                    led_test_pass_terminal(state)
                     state = 1
                 else:
-                    # fazer funçao de escrever no terminal
+                    led_test_error_terminal(state)
                     return -1
+
             if state == 1:
                 aux = 0
                 for i in range(0, len(leds_test)):
                     if vet_cor[i] is None:
                         aux = aux + 1
                 if aux == len(leds_test):
+                    led_test_pass_terminal(state)
                     state = 2
                 else:
-                    # fazer funçao de escrever no terminal
+                    led_test_error_terminal(state)
                     return -1
+
             if state == 2:
                 aux = 0
                 for i in range(0, len(leds_test)):
                     if vet_cor[i] is not None:
                         aux = aux + 1
                 if aux == len(leds_test):
+                    led_test_pass_terminal(state)
                     state = 3
                 else:
-                    # fazer funçao de escrever no terminal
+                    led_test_error_terminal(state)
                     return -1
+
             if state == 3:
                 aux = 0
-                #for i in range(0, len(leds_test)):
-                if vet_cor[0].get_name() == 'green' and vet_cor[0].get_name() != vet_cor_bef[0].get_name():
-
-
+                cam_bef = None
+                for i in range(0, 56):
+                    for j in range(0, len(leds_test)):
+                        if cam != cam_bef:
+                            vet_cor_bef[i][j] = HMIcv.led_test(cam, leds_test[i])
+                    cam_bef = cam
+                    cam = cam_value.get_image()
+                for i in range(0, 56):
+                    for j in range(0, len(leds_test)):
+                        for k in range(0, len(leds_test[j].get_colour())):
+                            if vet_cor_bef[i][j] == leds_test[j].get_colour()[k]:
+                                aux = aux + 1
+                            elif vet_cor_bef[i][j] != leds_test[j].get_colour()[k]:
 
     def test_display(self, display: DisplayTest, test):
         if test == 1:
@@ -261,3 +281,16 @@ class Test:
                 return 0
             else:
                 return -1
+
+def led_test_error_terminal(code):
+    if code == 0 or code == 2:
+        print("Error on turned on LED's\n")
+    elif code == 1:
+        print("Error on turned off LED's\n")
+
+
+def led_test_pass_terminal(code):
+    if code == 0 or code == 2:
+        print("All the leds Turn ON")
+    elif code == 1:
+        print("All the leds Turn OFF")
