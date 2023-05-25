727e061662c0fee3103a6f8348877b47715dfc46
José Francisco Ribeiro Carvalho <up201906240@edu.fe.up.pt>
2023-05-25 10:15:39 +0100

    added staticmethod and new overwrite to exitcode

--- src/main/python/com/hmi_test_system/report/exit_code.py
+++ src/main/python/com/hmi_test_system/report/exit_code.py
@@ -1,7 +1,12 @@
-
-exit_code = 0
-
 class ExitCode:
+    
+    _instance = None
+    
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
     def __init__(self):
         ExitCode.exit_code = 0
     
@@ -13,24 +18,31 @@ class ExitCode:
     def failure_model():
         ExitCode.update_value(2)
 
+    @staticmethod
     def invalid_arguments():
         ExitCode.update_value(3)
-        
+    
+    @staticmethod
     def invalid_number_arguments():
         ExitCode.update_value(4)
-        
+    
+    @staticmethod
     def failure_camera():
         ExitCode.update_value(5)
     
+    @staticmethod
     def failure_serial_port():
         ExitCode.update_value(6)
     
+    @staticmethod
     def failure_led_test():
         ExitCode.update_value(8)
     
+    @staticmethod
     def failure_display_test():
         ExitCode.update_value(9)
     
+    @staticmethod
     def failure_button_test():
         ExitCode.update_value(10)
     
