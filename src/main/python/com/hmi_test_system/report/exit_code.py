a95e74e6143686cc7ada89ea16470a8d40e39fd3
Renato <renatopjesus1818@gmail.com>
2023-05-25 09:24:41 +0100

    exit codes file update

--- src/main/python/com/hmi_test_system/report/exit_code.py
+++ src/main/python/com/hmi_test_system/report/exit_code.py
@@ -7,9 +7,34 @@ class ExitCode:
     
     @staticmethod
     def failure_excetution():
-        ExitCode.exit_code = 1
-        
+        ExitCode.update_value(1)
+
     @staticmethod
     def failure_model():
-        ExitCode.exit_code = 2
-    pass
+        ExitCode.update_value(2)
+
+    def invalid_arguments():
+        ExitCode.update_value(3)
+        
+    def invalid_number_arguments():
+        ExitCode.update_value(4)
+        
+    def failure_camera():
+        ExitCode.update_value(5)
+    
+    def failure_serial_port():
+        ExitCode.update_value(6)
+    
+    def failure_led_test():
+        ExitCode.update_value(8)
+    
+    def failure_display_test():
+        ExitCode.update_value(9)
+    
+    def failure_button_test():
+        ExitCode.update_value(10)
+    
+    @staticmethod
+    def update_value(new):
+        if ExitCode.exit_code is 0:
+            ExitCode.exit_code = new
