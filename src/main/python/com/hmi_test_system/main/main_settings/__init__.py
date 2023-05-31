8dcd2dec190a0931b43c9f68c1f8b6b5d73cce56
Renato <renatopjesus1818@gmail.com>
2023-05-31 23:26:15 +0100

    some import changes

--- src/main/python/com/hmi_test_system/main/main_settings/__init__.py
+++ src/main/python/com/hmi_test_system/main/main_settings/__init__.py
@@ -1,5 +1,7 @@
-__all__ = ['MenuPrints']
+__all__ = ['MenuPrints', 'new_model', 'settings_menu']
 
 from .menus import *
 from .menu_prints import MenuPrints
 from .library_settings import *
+from .menus import new_model
+from .menus import settings_menu
