3d9fc1a70f46fea59bcedf8f30b569625b02d196
Renato <renatopjesus1818@gmail.com>
2023-06-01 16:15:57 +0100

    correct main

--- src/main/python/com/hmi_test_system/main.py
+++ src/main/python/com/hmi_test_system/main.py
@@ -4,9 +4,11 @@ import sys
 from data.color.list_of_colors import ListOfColors
 from data.settings import Settings
 from main.constant_main import *
-from main.main_settings import *
-from main.main_settings.menu_prints import MenuPrints as MP
+from main.main_settings.menus import settings_menu
+#from main.main_settings import *
+#from main.main_settings.menu_prints import MenuPrints as MP
 from main.main_test.library_test import *
+from report import ExitCode
 
 '''from tkinter import Tk
 from tkinter.filedialog import askdirectory
@@ -38,7 +40,8 @@ if len(sys.argv) < 2:
     print("Miss arguments: main.py [type: set or test]")
     print("Ex: main.py set")
     print("Ex: main.py test [name_model] [(optional)type_test] [optionals]")
-    sys.exit()
+    ExitCode.failure_in_excetution()
+    sys.exit(ExitCode.get_current_value())
 
 value = sys.argv[1]
 if value == TYPE_TEST:
@@ -47,6 +50,7 @@ if value == TYPE_TEST:
     menu_choice = input('Press Enter')'''
     exit_code = test_menu(M)
     print("Test Exit Code =", exit_code)
+    
 elif value == TYPE_SET:
     # Menu Settings
     settings_menu(M)
@@ -56,7 +60,10 @@ elif value == TYPE_HELP:
 else:
     print("Wrong arguments, write main.py -help for help")
     menu_choice = input('Press Enter to exit')
-    
+
+
+sys.exit(ExitCode.get_current_value())
+
 '''while(settings):
     MP.main_menu_print()        
     menu_choice = input()
