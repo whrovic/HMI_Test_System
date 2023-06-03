b5f2dd63c0a97261f7f5d480b186009a4936ffde
Ricardo Silva <up201906290@g.uporto.pt>
2023-06-03 13:35:00 +0100

    Method seq_boot_loader_info in class SequenceTest

--- src/main/python/com/hmi_test_system/test/sequence_test.py
+++ src/main/python/com/hmi_test_system/test/sequence_test.py
@@ -56,7 +56,7 @@ class SequenceTest:
         # Start receiving data from serial port
         serial_port.start_receive()
 
-        print("Serial Port communication openned")
+        print("Serial Port communication opened")
 
         # Waits for serial port TestKeys begin
         begin_waiting_time = time()
@@ -101,7 +101,72 @@ class SequenceTest:
     
     @staticmethod
     def seq_boot_loader_info(model: Model, dsp = False):
-        return -1
+
+        # TODO: This shouldn't be defined here
+        TIMEOUT = 10
+
+        # Open the needed connections
+        SetupTest.setup(False, dsp, True, False)
+
+        # Check connections status
+        serial_port = SetupTest.__sp_main
+        if serial_port.closed():
+            # Couldn't open serial port connection
+            SetupTest.close()
+            ExitCode.serialport_connection_failure()
+            return -1
+        cam = SetupTest.__cam_display
+        if dsp and cam.closed():
+            # Couldn't open camera connection
+            SetupTest.close()
+            ExitCode.camera_connection_failure()
+            return -1
+        
+        # Start receiving data from serial port
+        serial_port.start_receive()
+
+        print("Serial Port communication opened")
+
+        # Waits for serial port TestKeys begin
+        begin_waiting_time = time()
+        received_sp = False
+        while True:
+            now = time()
+
+            # Check if the serial port timed out waiting for the first received data
+            if (not received_sp and (now - begin_waiting_time > TIMEOUT)):
+                # No data was received from the serial port
+                # TODO: Catch this error
+                SetupTest.close()
+                ExitCode.serialport_timeout_reception()
+                return -1
+            
+            # Get data from serial port
+            data, _ = serial_port.get_serial()
+
+            if data is not None:
+                received_sp = True
+                
+                data = str(data)
+                if data.startswith(TEST_BOOT_LOADER_INFO_BEGIN):
+                    break
+            
+            sleep(0.1)
+
+        # If the test will use both serial port and display, start recording images
+        if dsp:
+            cam.start_capture()
+
+        print("Bootloader Test started")
+
+        # Start button test
+        result = Test.test_boot_loader_info(cam, serial_port, model.get_boot_loader_info().get_version(), model.get_boot_loader_info().get_date())
+
+        # Close all the opened connections
+        SetupTest.close()
+
+        # Return -1 in case of error or 0 if success
+        return result
     
     @staticmethod
     def seq_board_info(model: Model, dsp = False):
@@ -151,6 +216,8 @@ class SequenceTest:
         
         # Start receiving data from serial port
         serial_port.start_receive()
+
+        print("Serial Port communication opened")
 
         # Waits for serial port TestKeys begin
         begin_waiting_time = time()
@@ -217,6 +284,8 @@ class SequenceTest:
         
         # Start receiving data from serial port
         serial_port.start_receive()
+
+        print("Serial Port communication opened")
 
         # Waits for serial port TestKeys begin
         begin_waiting_time = time()
