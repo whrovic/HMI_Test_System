eae43614bb6a8fa3a1e28e45acdb50476a6e8236
Ricardo Silva <up201906290@g.uporto.pt>
2023-06-03 14:05:02 +0100

    Updated class Info'attributes: serial number, manufacture date and lcd type

--- src/main/python/com/hmi_test_system/data/model/info.py
+++ src/main/python/com/hmi_test_system/data/model/info.py
@@ -1,13 +1,22 @@
 class Info:
     
-    def __init__(self, board: str, option: str, revision: str, edition: str):
+    def __init__(self, board: str, serial_number: str, manufacture_date: str, option: str, revision: str, edition: str, lcd_type: str):
         self._board = board
+        self.serial_number = serial_number
+        self.manufacture_date = manufacture_date
         self._option = option
         self._revision = revision
         self._edition = edition
+        self.lcd_type = lcd_type
 
     def get_board(self):
         return self._board
+    
+    def get_serial_number(self):
+        return self.serial_number
+    
+    def get_manufacture_date(self):
+        return self.manufacture_date
     
     def get_option(self):
         return self._option
@@ -17,3 +26,6 @@ class Info:
     
     def get_edition(self):
         return self._edition
+    
+    def get_lcd_type(self):
+        return self.lcd_type
