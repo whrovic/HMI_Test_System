5652ad25f8cf6e7c0e33b959a999399c5da9e564
Ricardo Silva <up201906290@g.uporto.pt>
2023-06-03 14:20:15 +0100

    Method seq_alight in class SequenceTest

--- src/main/python/com/hmi_test_system/test/sequence_test.py
+++ src/main/python/com/hmi_test_system/test/sequence_test.py
@@ -239,7 +239,72 @@ class SequenceTest:
     
     @staticmethod
     def seq_alight(dsp = False):
-        return -1
+        
+        # TODO: This shouldn't be defined here
+        TIMEOUT = 10
+
+        # Open the needed connections
+        SetupTest.setup(False, dsp, True, False)
+
+        # Check connections status
+        serial_port = SetupTest.__sp_main
+        if serial_port.closed():
+            # Couldn't open serial port connection
+            SetupTest.close()
+            ExitCode.serialport_connection_failure()
+            return -1
+        cam = SetupTest.__cam_display
+        if dsp and cam.closed():
+            # Couldn't open camera connection
+            SetupTest.close()
+            ExitCode.camera_connection_failure()
+            return -1
+        
+        # Start receiving data from serial port
+        serial_port.start_receive()
+
+        print("Serial Port communication opened")
+
+        # Waits for serial port TestKeys begin
+        begin_waiting_time = time()
+        received_sp = False
+        while True:
+            now = time()
+
+            # Check if the serial port timed out waiting for the first received data
+            if (not received_sp and (now - begin_waiting_time > TIMEOUT)):
+                # No data was received from the serial port
+                # TODO: Catch this error
+                SetupTest.close()
+                ExitCode.serialport_timeout_reception()
+                return -1
+            
+            # Get data from serial port
+            data, _ = serial_port.get_serial()
+
+            if data is not None:
+                received_sp = True
+                
+                data = str(data)
+                if data.startswith(TEST_ALIGHT_BEGIN):
+                    break
+            
+            sleep(0.1)
+
+        # If the test will use both serial port and display, start recording images
+        if dsp:
+            cam.start_capture()
+
+        print("Light Test started")
+
+        # Start button test
+        result = Test.test_alight(cam, serial_port)
+
+        # Close all the opened connections
+        SetupTest.close()
+
+        # Return -1 in case of error or 0 if success
+        return result
 
     @staticmethod
     def seq_led(model: Model, leds_test = None):
