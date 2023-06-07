509888c183bf3c3d1937b8342e83dbf1e9691feb
Flipardo <filipedasilvaloureiro@gmail.com>
2023-06-07 15:47:05 +0100

    library_test_settings updated

---
+++ src/main/python/com/hmi_test_system/main/main_settings/library_test_settings.py
@@ -0,0 +1,17 @@
+class LibraryTestSettings:
+
+    def add_camera():
+        pass
+
+    def edit_camera():
+        pass
+
+    def add_param_camera():
+        pass
+    
+
+    def add_sp():
+        pass
+
+    def edit_sp():
+        pass
