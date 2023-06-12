df60563f784742559c79ae2e3d12b028c1bad510
Renato <renatopjesus1818@gmail.com>
2023-06-12 19:50:46 +0100

    update names  menus

--- src/main/python/com/hmi_test_system/main/main_settings/menu_prints.py
+++ src/main/python/com/hmi_test_system/main/main_settings/menu_prints.py
@@ -4,7 +4,7 @@ import os
 class MenuPrints:
 
     @staticmethod
-    def settings():
+    def sett():
         os.system('cls')
         print("------------ Menu Settings ------------\n\n")
         print("1- New model        2- Color           \n")
@@ -13,7 +13,7 @@ class MenuPrints:
         print("---------------------------------------\n")
     
     @staticmethod
-    def new_model(): #sett_newmodel
+    def sett_newmodel():
         os.system('cls')
         print("-------------- New Model --------------\n\n")
         print("1- Create           2- Import XML   \n")
@@ -21,15 +21,7 @@ class MenuPrints:
         print("---------------------------------------\n")
     
     @staticmethod
-    def test_settings(): #sett_testsettings
-        os.system('cls')
-        print("------------ Test Settings ------------\n\n")
-        print("1- Camera           2- Serial Port     \n")
-        print("3- Back             4- Exit            \n\n")
-        print("---------------------------------------\n")
-    
-    @staticmethod
-    def color(): #sett_color
+    def sett_color():
         os.system('cls')
         print("------------ Color Settings -----------\n\n")
         print("1 - Edit Colors      2 - Add Color     \n")
@@ -38,7 +30,7 @@ class MenuPrints:
         print("---------------------------------------\n")
 
     @staticmethod
-    def edit_color(): #sett_color_editcolor
+    def sett_color_editcolor(): 
         os.system('cls')
         print("--------- Edit Color Settings ---------\n\n")
         print("1 - Edit name        2 - Edit 1st range\n")
@@ -47,26 +39,17 @@ class MenuPrints:
         print("---------------------------------------\n")
 
     @staticmethod
-    def directory(): #sett_directory
-        os.system('cls')
-        print("------------ Test Settings ------------\n\n")
-        print("1- Settings         2- Resource     \n")
-        print("               3- XML                 \n")
-        print("4- Back             5- Exit            \n\n")
-        print("---------------------------------------\n")
-    
-    @staticmethod
-    def edit_menu(): #sett_editmenu
+    def sett_editmenu():
         os.system('cls')
         print("-------------- Edit Menu --------------\n\n")
         print("1- Edit model info  2- Edit led        \n")
-        print("3- Edit button      4- Edit LCD        \n")
+        print("3- Edit button      4- Edit Display    \n")
         print("               5- Save                 \n")
         print("6- Back             7- Exit            \n\n")
         print("---------------------------------------\n")
 
     @staticmethod
-    def edit_led(): #sett_editmenu_editled
+    def sett_editmenu_editled(): 
         os.system('cls') 
         print("--------------- Edit Led --------------\n\n")
         print("1- Name             2- Colours         \n")
@@ -74,10 +57,27 @@ class MenuPrints:
         print("---------------------------------------\n")
     
     @staticmethod
-    def edit_button(): #sett_editmenu_editbutton
+    def sett_editmenu_editbutton(): 
         os.system('cls') 
         print("------------- Edit button -------------\n\n")
         print("1- Name             2- Position        \n")
         print("               3- Back                 \n\n")
+        print("---------------------------------------\n")
+    
+    @staticmethod
+    def sett_testsettings(): 
+        os.system('cls')
+        print("------------ Test Settings ------------\n\n")
+        print("1- Camera           2- Serial Port     \n")
+        print("3- Back             4- Exit            \n\n")
+        print("---------------------------------------\n")
+    
+    @staticmethod
+    def sett_directory(): 
+        os.system('cls')
+        print("------------ Test Settings ------------\n\n")
+        print("1- Settings         2- Resource     \n")
+        print("               3- XML                 \n")
+        print("4- Back             5- Exit            \n\n")
         print("---------------------------------------\n")
     
--- src/main/python/com/hmi_test_system/main/main_settings/menu_settings.py
+++ src/main/python/com/hmi_test_system/main/main_settings/menu_settings.py
@@ -3,117 +3,177 @@ import os
 from data.define_and_fill_model import DefineAndFillModel as df
 from data.settings import Settings
 from main.constant_main import *
-from main.library import Library as L
+from main.library import Library as Lib
 
 from .library_color import LibraryColor as LC
 from .library_directory import LibraryDirectory as LD
 from .library_edit_model import LibraryEditModel as LEM
 from .library_new_model import LibraryNewModel as LNM
 from .menu_prints import MenuPrints as MP
+from data.color.list_of_colors import ListOfColors
 
 
 class MenuSettings:
 
     @staticmethod
-    def settings_menu():
+    def sett():
         count = 0
         while True:
-            MP.settings()
+            MP.sett()
             menu_choice = input().strip()
-            
-            if len(menu_choice) == 0:
-                continue
+            if len(menu_choice) == 0: continue
 
             match (menu_choice):
                 #new model
                 case '1':
                     count = 0
-                    if ( MenuSettings.new_model_menu() == -1 ):
+                    if ( MenuSettings.sett_newmodel() == -1 ):
+                        os.system('cls')
                         return -1
-                
                 # color
                 case '2':                    
                     count = 0
-                    if (MenuSettings.edit_color_menu() == -1):
+                    if (MenuSettings.sett_color() == -1):
+                        os.system('cls')
                         return -1
-            
                 # edit model 
                 case '3':
                     count = 0
-                    if ( MenuSettings.edit_model_menu() == -1 ):
+                    if (MenuSettings.sett_editmenu() == -1 ):
+                        os.system('cls')
                         return -1
-
                 # test setting
                 case '4':
                     count = 0
-                    if ( MenuSettings.edit_test_settings() == -1 ):
+                    if (MenuSettings.sett_testsettings() == -1 ):
+                        os.system('cls')
                         return -1
-            
                 # directory
                 case '5':
                     count = 0
-                    if ( MenuSettings.edit_directory_menu() == -1 ):
+                    if ( MenuSettings.sett_directory() == -1 ):
+                        os.system('cls')
                         return -1
-
                 # exit 
                 case '6':
                     os.system('cls')
                     return 0
-            
                 case _:
                     count = count + 1
-                    if (count > NTIMEOUT_MENUS):
+                    if (count > NTIMEOUT_MENUS): 
+                        os.system('cls')
                         return -1
-                    L.exit_input("Invalid input")
+                    Lib.exit_input("Invalid input")
                     continue
     
     @staticmethod
-    def new_model_menu():
+    def sett_newmodel():
         count = 0
         while True:
-            
-            MP.new_model()
+            MP.sett_newmodel()
             menu_choice = input()
+            if len(menu_choice) == 0: continue
             
             match (menu_choice):
                 # create
                 case '1':
                     count = 0
-                    LNM.new_model_mannually()
-
+                    LNM.new_model_create()
                 # Import XML  
                 case '2':
                     count = 0
                     LNM.new_model_import_xml()
-            
                 # back  
                 case '3':
-                    os.system('cls')
                     return 0
-
                 # exit  
                 case '4':
-                    os.system('cls')
                     return -1
-                
                 case _:
                     count = count + 1
-                    if (count > NTIMEOUT_MENUS):
-                        return -1
-                    L.exit_input("Invalid input")
+                    if (count > NTIMEOUT_MENUS):  return -1
+                    Lib.exit_input("Invalid input")
                     continue
     
     @staticmethod
-    def edit_model_menu():
+    def sett_color():
+        count = 0
+        while True:
+            MP.sett_color()
+            menu_choice = input()
+            if len(menu_choice) == 0: continue
+
+            match (menu_choice):
+                # Edit colors
+                case '1':
+                    count = 0
+                    MenuSettings.sett_color_editcolors()
+                # New color
+                case '2':
+                    count = 0
+                    LC.edit_color_new_color()
+                # Delete color
+                case '3':
+                    count = 0
+                    LC.edit_color_delete_color()
+                # Back
+                case '4':
+                    return 0
+                # Exit
+                case '5':
+                    return -1
+                case _:
+                    count += 1
+                    if (count > NTIMEOUT_MENUS):  return -1
+                    Lib.exit_input("Invalid input")
+                    continue
+
+
+    @staticmethod
+    def sett_color_editcolors():
+        color = LC.sett_color_editcolor_first()
+        if color is None: return -1
+        count = 0
+        while True:
+            
+            MP.sett_color_editcolor()
+            menu_choice = input().strip()
+            if len(menu_choice) == 0: continue
+
+            match (menu_choice):
+                # Edit name
+                case '1':
+                    LC.edit_color_edit_color_edit_name(color)
+                # Edit 1st range
+                case '2':
+                    LC.edit_color_edit_color_edit_1st_range(color)
+                # Edit 2nd range
+                case '3':
+                    LC.edit_color_edit_color_edit_2nd_range(color)
+                # Delete
+                case '4':
+                    LC.edit_color_edit_color_edit_delete(color)
+                # Back
+                case '5':
+                    break
+                case _:
+                    count += 1
+                    if (count >= NTIMEOUT_MENUS): return -1
+                    Lib.exit_input("Invalid input")
+                    continue
+        return 0
+    
+    @staticmethod
+    def sett_editmenu():
         
-        model = MenuSettings.edit_model_choose_model()
+        model = LEM.sett_editmenu_first()
         if model is None:
             return 0
         
         count = 0        
         while True:
             
-            MP.edit_menu()
+            MP.sett_editmenu()
             menu_choice = input()
 
             match (menu_choice):
@@ -136,7 +196,7 @@ class MenuSettings:
                 # save
                 case '5':
                     df.create_xml(model)
-                    L.exit_input("Changes saved!")
+                    Lib.exit_input("Changes saved!")
                     continue
                 
                 #back
@@ -153,87 +213,15 @@ class MenuSettings:
                     count = count + 1
                     if (count > NTIMEOUT_MENUS):
                         return -1
-                    L.exit_input("Invalid input")
+                    Lib.exit_input("Invalid input")
                     continue
 
     @staticmethod
-    def edit_model_choose_model():
-        
-        os.system('cls')
-
-        # Get list of available models
-        name_models = df.get_all_xml_file_names()
-        if name_models is None:
-            # TODO: Error code
-            L.exit_input("Error path don't exist")
-            return None
-        elif len(name_models) == 0:
-            L.exit_input("No available models to edit")
-            return None
-        
-        # Choose which model to edit
-        while True:
-
-            os.system('cls') 
-
-            # Print all available models
-            print("Available models:")
-            for i, name in enumerate(name_models):
-                print(str(i+1) + ' - ' + name)
-            
-            name_model=L.get_name_or_index("\nWhat model do you want to edit?", name_models)
-            if (name_model is None):
-                # back to menu
-                return None
-                        
-            if (df.open_model_xml(name_model) is None):
-                os.system('cls')
-                L.exit_input(f"{name_model} doesn't exist\n")
-                continue
-            else:
-                return Settings.get_model(name_model)
-    
-    #TODO
-    @staticmethod
-    def edit_color_menu():
-        
-        count = 0
-        while True:
-
-            MP.color()
-            menu_choice = input()
-
-            match (menu_choice):
-                # Edit colors
-                case '1':
-                    count = 0
-                    LC.edit_color_edit_color()
-                # New color
-                case '2':
-                    count = 0
-                    LC.edit_color_new_color()
-                # Delete color
-                case '3':
-                    count = 0
-                    LC.edit_color_delete_color()
-                # Back
-                case '4':
-                    return 0
-                # Exit
-                case '5':
-                    return -1
-                case _:
-                    count += 1
-                    if (count > NTIMEOUT_MENUS):
-                        return -1
-
-    
-    @staticmethod
-    def edit_test_settings():
+    def sett_testsettings():
         count = 0
         while True:
             
-            MP.test_settings()        
+            MP.sett_testsettings()        
             menu_choice = input()
             
             match (menu_choice):
@@ -263,13 +251,15 @@ class MenuSettings:
                     count = count + 1
                     if (count > NTIMEOUT_MENUS):
                         return -1
+                    Lib.exit_input("Invalid input")
+                    continue
 
     # TODO
     @staticmethod
-    def edit_directory_menu():
+    def sett_directory():
         count = 0
         while True:
-            MP.directory()        
+            MP.sett_directory()        
             menu_choice = input()
             
             match (menu_choice):
@@ -306,5 +296,5 @@ class MenuSettings:
                     count = count + 1
                     if (count > NTIMEOUT_MENUS):
                         return -1
-                    L.exit_input("Invalid input")
+                    Lib.exit_input("Invalid input")
                     continue
