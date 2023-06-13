46e353e9a28345ee6bee6d29018e5543d312e310
Renato <renatopjesus1818@gmail.com>
2023-06-13 16:35:57 +0100

    some corrections

--- src/main/python/com/hmi_test_system/data/model/info.py
+++ src/main/python/com/hmi_test_system/data/model/info.py
@@ -10,15 +10,29 @@ class Info:
     def get_board(self):
         return self._board
     
+    def set_board(self, board: str):
+        self._board = board
+    
     def get_option(self):
         return self._option
     
+    def set_option(self, option: str):
+        self._option = option
+        
     def get_revision(self):
         return self._revision
     
+    def set_revision(self, revision: str):
+        self._revision = revision
+        
     def get_edition(self):
         return self._edition
     
+    def set_edition(self, edition: str):
+        self._edition = edition
+        
     def get_lcd_type(self):
         return self._lcd_type
     
+    def set_lcd_type(self, lcd_type: str):
+        self._lcd_type = lcd_type
