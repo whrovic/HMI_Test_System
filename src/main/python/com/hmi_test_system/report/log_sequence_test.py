f91c906da85f9485f623b5958b18c987ae3d6cc5
Ricardo Silva <up201906290@g.uporto.pt>
2023-06-15 12:14:43 +0100

    Finish Sequence Test logs

--- src/main/python/com/hmi_test_system/report/log_sequence_test.py
+++ src/main/python/com/hmi_test_system/report/log_sequence_test.py
@@ -7,9 +7,37 @@ class LogSequenceTest(Log):
         Log.info_log("Invalid Parameter: Parameter is None")
 
     @staticmethod
+    def image_none():
+        Log.info_log("Images Reference is None")
+
+    @staticmethod
     def button_not_found():
         Log.info_log("Button not found")
 
     @staticmethod
     def led_not_found():
         Log.info_log("Led not found")
+
+    @staticmethod
+    def start_buttons_test():
+        Log.info_log("Buttons Tests started")
+
+    @staticmethod
+    def start_board_info_test():
+        Log.info_log("Board Info Test started")
+
+    @staticmethod
+    def start_boot_loader_test():
+        Log.info_log("Bootloader Test started")
+
+    @staticmethod
+    def start_test_alight():
+        Log.info_log("Light Test started")
+
+    @staticmethod
+    def start_test_leds():
+        Log.info_log("Leds Tests started")
+
+    @staticmethod
+    def start_test_display():
+        Log.info_log("Display Tests started")
